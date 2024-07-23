72d0f2750a806fb9c70eb66a843788b03c19b8e0
e.mittendorfer <elisabeth.mitten@gmail.com>
2024-07-23 14:49:57 +0200

    add wall collision and scoreboard

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from snake import Snake
 from food import Food
 import time
 
+from soreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,13 +13,13 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
 
 game_is_on = True
 while game_is_on:
@@ -27,10 +28,14 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    #Detect collision
+    # Detect food collision
     if snake.head.distance(food) < 20:
+        scoreboard.increase_score()
         food.move_to_random_position()
 
+    # Detect wall collision
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
 screen.exitonclick()
-
-
